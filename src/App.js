74783aeb6f0e08e8d0dd98c38937673249b61e20
Arc Coder | Harsh Patel <dev@arccoder.in>
2020-11-29 12:19:21 +0530

    Final outcome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import spaceCharacters from "./data";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [characters, setCharacters] = useState(spaceCharacters);
+
+    function handleOnDragEnd(result) {
+        if (!result.destination) return;
+
+        const items = Array.from(characters);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+
+        setCharacters(items);
+    }
+
+    return (
+        <div className="main__container">
+            <header className="body__container">
+                <h1>Final Space Characters</h1>
+                <DragDropContext onDragEnd={handleOnDragEnd}>
+                    <Droppable droppableId="characters">
+                        {(provided) => (
+                            <ul
+                                className="characters"
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}
+                            >
+                                {characters.map(
+                                    ({ id, name, thumb }, index) => {
+                                        return (
+                                            <Draggable
+                                                key={id}
+                                                draggableId={id}
+                                                index={index}
+                                            >
+                                                {(provided) => (
+                                                    <li
+                                                        ref={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                    >
+                                                        <div
+                                                            className="characters__image"
+                                                            {...provided.dragHandleProps}
+                                                        >
+                                                            <img
+                                                                src={thumb}
+                                                                alt={`${name} Thumb`}
+                                                            />
+                                                        </div>
+                                                        <p>{name}</p>
+                                                    </li>
+                                                )}
+                                            </Draggable>
+                                        );
+                                    }
+                                )}
+                                {provided.placeholder}
+                            </ul>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+            </header>
+            <p>
+                Images from{" "}
+                <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">
+                    Final Space Wiki
+                </a>
+            </p>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const characters = [
+    {
+        id: "gary",
+        name: "Gary Goodspeed",
+        thumb: "./assets/gary.png",
+    },
+    {
+        id: "cato",
+        name: "Little Cato",
+        thumb: "./assets/cato.png",
+    },
+    {
+        id: "kvn",
+        name: "KVN",
+        thumb: "./assets/kvn.png",
+    },
+    {
+        id: "mooncake",
+        name: "Mooncake",
+        thumb: "./assets/mooncake.png",
+    },
+    {
+        id: "quinn",
+        name: "Quinn Ergon",
+        thumb: "./assets/quinn.png",
+    },
+];
+
+export default characters;
